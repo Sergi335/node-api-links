70ee1f6d90f9458213b589195001ee623e9b4982
Sergio <sergiadn335@gmail.com>
2024-01-17 18:22:30 +0100

    change on middlewares/session.js

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,10 +1,24 @@
-export function attachCsrfToken (url, cookie, value) {
-  return function (req, res, next) {
-    console.log(url)
-    console.log(req.url)
-    if (req.url === url) {
-      res.cookie(cookie, value)
-    }
+// export function attachCsrfToken (url, cookie, value) {
+//   return function (req, res, next) {
+//     console.log(url)
+//     console.log(req.url)
+//     if (req.url === url) {
+//       res.cookie(cookie, value)
+//     }
+//     next()
+//   }
+// }
+export const attachCsrfToken = (route, cookieName, csrfToken) => {
+  return (req, res, next) => {
+    // Configura la cookie con SameSite=None si es necesario (asegúrate de que tu conexión sea HTTPS)
+    const isSecure = req.secure || (req.headers['x-forwarded-proto'] === 'https')
+    const sameSiteConfig = isSecure ? 'None' : 'Lax' // Puedes ajustar esto según tus necesidades
+
+    res.cookie(cookieName, csrfToken, {
+      sameSite: sameSiteConfig,
+      secure: isSecure
+    })
+
     next()
   }
 }
