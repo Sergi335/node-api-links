adf742840f4d8fcae69523e6b4e72e4c2c07ebc7
Sergio <sergiadn335@gmail.com>
2024-01-24 16:09:17 +0100

    Change on cookies

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -146,7 +146,7 @@ export const sessionCookieMiddleware = async (req, res, next) => {
       .then(
         (sessionCookie) => {
           // Set cookie policy for session cookie.
-          const options = { maxAge: expiresIn, httpOnly: true, secure: true }
+          const options = { maxAge: expiresIn, httpOnly: true, secure: false }
           console.log('Cookie creada con éxito')
           res.cookie('session', sessionCookie, options)
           next()
